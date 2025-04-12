591792debc2b2c937afe124af1f081114de3d16b
Polina Barinova <98588978+PolinaBarinova@users.noreply.github.com>
2025-04-12 19:19:04 +0300

    Update Main.java

--- src/main/java/qa/test/Main.java
+++ src/main/java/qa/test/Main.java
@@ -2,6 +2,6 @@ package qa.test;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour le monde!");
+        System.out.println("Привет, мир!");
     }
 }
